ea5820a461afbcb12c49594e99c5abee96f326bd
kyle-mucerino <kyle.mucerino@gmail.com>
2023-03-24 13:42:53 -0700

    added store.js file to db folder and finished code for apiRoutes.js and htmlRoutes.js

--- Develop/server.js
+++ Develop/server.js
@@ -3,13 +3,13 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001; 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
-app.use('/html', htmlRoutes);
+app.use(htmlRoutes);
 
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
 
