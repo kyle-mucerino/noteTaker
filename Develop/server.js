ec87641f484459ea51b4b288213e06b5ab78feb6
kyle-mucerino <kyle.mucerino@gmail.com>
2023-03-24 09:49:31 -0700

    set up Middleware for parsing JSON and urlencoded form data in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,16 @@
-const express = require("express");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/html', htmlRoutes);
+
+app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`));
+
 
